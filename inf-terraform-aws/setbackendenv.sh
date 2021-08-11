e1d5c463e3f55cfef2557a013a5878574deac00b
Wais,Erhard (IT INF) BIG-AT-V <erhard.wais@boehringer-ingelheim.com>
2021-08-11 16:05:23 +0200

    initial commit

---
+++ inf-terraform-aws/setbackendenv.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+##################################################
+## This script can be used to set required environment
+## paramaters for the Terraform backend in order to
+## use the AWS Quickstarter outside of ODS.
+###################################################
+# params:
+#   PROJECT
+#   COMPONENT
+#   ENVIRONEMT
+#
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -p|--project)
+      PROJECT="$2"
+      shift; shift
+      ;;
+    -c|--component)
+      COMPONENT="$2"
+      shift; shift
+      ;;
+    -e|--environment)
+      ENVIRONMENT="$2"
+      shift; shift
+      ;;
+    *)    # unknown option
+      exit 1
+     ;;
+  esac
+done
+
+
+ACCOUNT=$(aws sts  get-caller-identity --output text)
+set -- $ACCOUNT
+
+export TF_BACKEND_PREFIX=$1
+export TF_BACKEND_S3KEY=$1/$PROJECT/$PROJECT-$COMPONENT/$ENVIRONMENT
+export TF_WORKSPACE=DEVELOPMENT
+export INSTALL_REPORT_HOME=.
+
+echo "TF_BACKEND_PREFIX="$TF_BACKEND_PREFIX
+echo "TF_BACKEND_S3KEY ="$TF_BACKEND_S3KEY
+
